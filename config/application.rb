9e2c89f8ee7de5365040360ffee9945c9cecb388
Jon Evans <jle@foraker.com>
2015-10-02 15:09:45 -0600

    Configure session store so Omniauth can work

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,11 @@ module IdeanearingApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_after ActiveRecord::QueryCache, ActionDispatch::Cookies
+
+    config.middleware.insert_after \
+      ActionDispatch::Cookies, ActionDispatch::Session::CookieStore,
+      key: '_ideaneering_session'
   end
 end
